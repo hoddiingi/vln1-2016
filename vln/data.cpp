eab07fddaeb940ec6f10e0031a7a46915884df67
Magnus Magnusson <Magnus Magnusson>
2016-11-29 13:38:21 +0000

    Vector

--- vln/data.cpp
+++ vln/data.cpp
@@ -1,6 +1,28 @@
 #include "data.h"
+#include <fstream>
+#include <vector>
+
+
 
 Data::Data()
 {
 
+}
+
+vector<Data> Data::readData()
+{
+    vector<Data> readData;
+
+    readData r("Alan Turing", "Male", 1912, 1954);
+
+    ofstream writeText;
+    writeText.open("text.txt")
+
+    readData.push_back(r);
+
+    writeText << r;
+
+    writeText.close();
+
+    return readData;
 }
--- vln/data.h
+++ vln/data.h
@@ -1,11 +1,14 @@
 #ifndef DATA_H
 #define DATA_H
+#include <vector>
 
 
 class Data
 {
 public:
     Data();
+
+    vector<Data> readData;
 };
 
 #endif // DATA_H
