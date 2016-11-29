af47d7611ad57de5585eec5d004b822520645457
Hörður Ingi Kristjánsson <Hrur Ingi Kristjnsson>
2016-11-29 16:50:39 +0000

    laga

--- vln/domain.cpp
+++ vln/domain.cpp
@@ -16,12 +16,9 @@ vector<Person> Domain::readData()
     vector<Person> returnData;
     //returnData[Person::getBirth()]
     //returnData.push_back(Person::getBirth())
-<<<<<<< HEAD
-=======
 
    return returnData;
 }
->>>>>>> 6ece16006aa2d1680a6068c4ba08f9821ecf7241
 
 void Domain::writeData(Person p)
 {
