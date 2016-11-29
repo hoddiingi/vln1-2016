7073c3ade6e143c598776af26facb214ae0eabd6
Anton Freyr Arnarsson <antonfreyr@Antons-MacBook-Pro-2.local>
2016-11-29 15:18:18 +0000

    comment

--- vln/data.cpp
+++ vln/data.cpp
@@ -12,21 +12,3 @@ Data::Data()
 {
 
 }
-
-
-<<<<<<< HEAD
-    //readData.push_back(r);
-
-    //writeText << r;
-
-    //writeText.close();
-
-    return readData;
-}
-
-int Data::ifSorting()
-{
-
-}
-=======
->>>>>>> 0b660433e6fffb003b87000af0098e53d92bab8d
--- vln/data.h
+++ vln/data.h
@@ -10,7 +10,6 @@ public:
     Data();
 
 
-
 private:
 
 
