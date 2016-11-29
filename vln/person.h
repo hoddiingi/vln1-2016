2cb3a5db8a17e186878df4a35088ec8b0017742d
audurr16 <audurr16@ru.is>
2016-11-29 12:29:24 +0000

    útfærsla á person klasa

--- vln/person.h
+++ vln/person.h
@@ -8,6 +8,10 @@ class Person
 {
 public:
     Person();
+    string getName() const;
+    char getGender() const;
+    int getBirth() const;
+    int getDeath() const;
 
 private:
     string _name;
