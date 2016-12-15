222beb8c292085a5b11fe2f99c8f3a338fedf561
thelmat16 <thelmat16@ru.is>
2016-12-15 09:04:00 +0000

    search connection fjarlægt

--- vln-vika3/vln3/mainwindow.cpp
+++ vln-vika3/vln3/mainwindow.cpp
@@ -199,16 +199,6 @@ void MainWindow::on_input_filter_computers_textChanged(const QString &arg1)
     vector<Computer> computers = _dom.searchComputer(userInput);
     displayComputers(computers);
 }
-void MainWindow::on_input_filter_connections_textChanged(const QString &arg1)
-{
-    QString userInput = ui->input_filter_computers->text();
-
-    vector<Person> ID = _dom.searchName(userInput);
-    displayAllConnections();
-    //Person p = ID[0];
-    //QString scientistName = QString::fromStdString(p.getName());
-
-}
 
 void MainWindow::on_button_edit_scientist_clicked()
 {
@@ -299,7 +289,6 @@ void MainWindow::on_button_remove_connections_clicked()
 
     if(success)
     {
-        ui->input_filter_connections->setText("");
         displayAllConnections();
 
         ui->button_remove_connections->setEnabled(false);
