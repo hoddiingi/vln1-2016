dd32b4a8105c886997a6afcbc5ae94b7f1a92cc5
audurr16 <audurr16@ru.is>
2016-12-15 14:53:58 +0000

    yfirferð á kóða

--- vln-vika3/vln3/mainwindow.h
+++ vln-vika3/vln3/mainwindow.h
@@ -25,41 +25,40 @@ public:
 private slots:
     void on_table_computers_clicked(const QModelIndex &index);              //For button edit and remove
     void on_table_scientists_clicked(const QModelIndex &index);             //For button edit and remove
+    void on_table_connections_clicked(const QModelIndex &index);            //For button remove
     void on_button_add_scientist_clicked();                                 //For when we click to add a scientist
     void on_button_add_computer_clicked();                                  //For when we click to add a computer
+    void on_button_add_connections_clicked();                               //For when we click to add a connections
     void on_input_filter_scientist_textChanged(const QString &arg1);        //For search a scientist
     void on_input_filter_computers_textChanged(const QString &arg1);        //For search a computer
     void on_button_edit_scientist_clicked();                                //For edit scientist
-    void on_button_remove_computer_clicked();                               //For remove one computer
-    void on_button_remove_scientist_clicked();                              //For remove one scientist
-    void on_button_remove_connections_clicked();                            //For remove one connection
-    void on_button_removeAll_computers_clicked();                           //For remove all computer
-    void on_button_removeAll_scientists_clicked();                          //For remove all scientists
-    void on_table_connections_clicked(const QModelIndex &index);            //For button edit and remove
-    void on_button_add_connections_clicked();                               //For add a connections
     void on_button_edit_computer_clicked();                                 //For edit a computer
+    void on_button_remove_scientist_clicked();                              //For remove one scientist
+    void on_button_remove_computer_clicked();                               //For remove one computer
+    void on_button_remove_connections_clicked();                            //For remove one connection
+    void on_button_removeAll_scientists_clicked();                          //For remove all scientists
+    void on_button_removeAll_computers_clicked();                           //For remove all computer
     void on_button_removeAll_connections_clicked();                         //For remove all connections
-
     void on_pushButton_2_clicked(bool checked);
-
     void on_pushButton_clicked(bool checked);
 
 private:
 
-    void displayAllComputers();                                     //Display all computers from SQLite
-    void displayComputers(std::vector<Computer> computers);         //Gets computer from vector to display
+
     void displayAllScientists();                                    //Display all scientists from SQLite
     void displayScientists(std::vector<Person> scientists);         //Gets scientist from vector to display
+    void displayAllComputers();                                     //Display all computers from SQLite
+    void displayComputers(std::vector<Computer> computers);         //Gets computer from vector to display
     void displayAllConnections();                                   //Display all connections from SQLite
     void displayConnections(std::vector<Connection> connections);   //Gets connection from vector to display
 
-    vector<Computer> _currentlyDisplayedComputer;                   //Private member variables
-    vector<Person> _currentlyDisplayedScientist;
+    vector<Person> _currentlyDisplayedScientist;                    //Private member variables
+    vector<Computer> _currentlyDisplayedComputer;
     vector<Connection> _currentlyDisplayedConnection;
     Person _persons;
     Domain _dom;
     Data _dat;
-    //editscientistsdialog _editSci;
+
     Ui::MainWindow *ui;
 };
 
